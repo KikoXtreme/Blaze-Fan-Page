0b4b01f707b8d7c839e05f9da1bf9509f44076c4
Kiril Valkov <kiril_dgr@abv.bg>
2022-11-30 21:06:46 +0200

    contacts test update

--- blaze-fan-page/src/components/Main/Contacts/Contacts.js
+++ blaze-fan-page/src/components/Main/Contacts/Contacts.js
@@ -1,5 +1,12 @@
-export const Contacts = ()=>{
-    return(
-        <p>Contacts</p>
+import { useState } from 'react';
+
+export const Contacts = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <p>Contacts</p>
+            <p>Test Counts: {count} clicks</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
     );
 }
